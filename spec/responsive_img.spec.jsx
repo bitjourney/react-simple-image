45811a1fb1d6cfb46e8e24b380cf8d29b52ac571
Kenju Wagatsuma <ken901waga@gmail.com>
2016-12-24 14:41:50 +0900

    Update spec to accept both pixel/width desciptor

--- spec/responsive_img.spec.jsx
+++ spec/responsive_img.spec.jsx
@@ -5,18 +5,69 @@ import { createElement } from 'react';
 import ResponsiveImg from 'responsive_img';
 
 describe('ResponsiveImg', () => {
-    it('renders expected html string', () => {
+    it('renders an expected html string with width descriptor', () => {
         const props = {
-            alt: 'foo',
-            srcSet: {
-                '1x': 'foo.svg',
-                '2x': 'foo@2x.svg'
-            }
+            alt: 'example',
+            widthDescriptor:[
+                {
+                    descriptor: '360w',
+                    src: 'example-small.svg',
+                },
+                {
+                    descriptor: '720w',
+                    src: 'example-middle.svg',
+                },
+                {
+                    descriptor: '1200w',
+                    src: 'example-large.svg',
+                },
+            ],
         };
         const html = renderToString(createElement(ResponsiveImg, props));
         assert(html.startsWith('<img'));
-        assert(html.includes(' alt="foo" '));
-        assert(html.includes(' src="foo.svg" '));
-        assert(html.includes(' srcset="foo.svg 1x,foo@2x.svg 2x" '));
+        assert(html.includes(' alt="example" '));
+        assert(html.includes(' src="example-small.svg" '));
+        assert(html.includes(' srcset="example-small.svg 360w, example-middle.svg 720w, example-large.svg 1200w" '));
+    });
+
+    it('renders an expected html string with pixel descriptor', () => {
+        const props = {
+            alt: 'example',
+            pixelDescriptor : [
+                {
+                    descriptor: '1x',
+                    src: 'example.svg'
+                },
+                {
+                    descriptor: '2x',
+                    src: 'example@2x.svg'
+                },
+            ],
+        };
+        const html = renderToString(createElement(ResponsiveImg, props));
+        assert(html.startsWith('<img'));
+        assert(html.includes(' alt="example" '));
+        assert(html.includes(' src="example.svg" '));
+        assert(html.includes(' srcset="example.svg 1x, example@2x.svg 2x" '));
+    });
+
+    it('should throw error if both width descriptor and pixel descriptor are given', () => {
+        const props = {
+            alt: 'foo',
+            pixelDescriptor : [
+                {
+                    descriptor: '1x',
+                    src: 'example.svg'
+                },
+            ],
+            widthDescriptor:[
+                {
+                    descriptor: '360w',
+                    src: 'example-small.svg',
+                },
+            ],
+        };
+        const html = renderToString(createElement(ResponsiveImg, props));
+        // TODO: add test to catch console warning
     });
 });
