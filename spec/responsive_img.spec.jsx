cde7744ba18aea5e7dd9da999379884434aad5b9
Kenju Wagatsuma <ken901waga@gmail.com>
2016-12-24 12:19:08 +0900

    Add gitignore

---
+++ spec/responsive_img.spec.jsx
@@ -0,0 +1,22 @@
+import assert from 'power-assert';
+import { renderToString } from 'react-dom/server';
+import { createElement } from 'components';
+
+import ResponsiveImg from 'components/shared/responsive_img';
+
+describe('ResponsiveImg', () => {
+    it('renders expected html string', () => {
+        const props = {
+            alt: 'foo',
+            srcSet: {
+                '1x': 'foo.svg',
+                '2x': 'foo@2x.svg'
+            }
+        };
+        const html = renderToString(createElement(ResponsiveImg, props));
+        assert(html.startsWith('<img'));
+        assert(html.includes(' alt="foo" '));
+        assert(html.includes(' src="foo.svg" '));
+        assert(html.includes(' srcset="foo.svg 1x,foo@2x.svg 2x" '));
+    });
+});
