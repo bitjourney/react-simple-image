b5d785ab6d6daaf6ff764ecbdd7401ba1e90c49e
Kenju Wagatsuma <ken901waga@gmail.com>
2016-12-24 15:15:49 +0900

    Update indent size from 4 to 2

--- spec/responsive_img.spec.jsx
+++ spec/responsive_img.spec.jsx
@@ -5,87 +5,87 @@ import {createElement} from 'react';
 import ResponsiveImg from 'responsive_img';
 
 describe('ResponsiveImg', () => {
-    describe('with widthDescriptor', () => {
+  describe('with widthDescriptor', () => {
 
-        it('renders an expected html string without sizes option', () => {
-            const props = {
-                alt: 'example',
-                widthDescriptor: {
-                    srcSet: [
-                        {descriptor: '360w', src: 'example-small.svg',},
-                        {descriptor: '720w', src: 'example-middle.svg',},
-                        {descriptor: '1200w', src: 'example-large.svg',},
-                    ],
-                },
-            };
-            const html = renderToString(createElement(ResponsiveImg, props));
-            assert(html.startsWith('<img'));
-            assert(html.includes(' alt="example" '));
-            assert(html.includes(' src="example-small.svg" '));
-            assert(html.includes(' srcset="example-small.svg 360w, example-middle.svg 720w, example-large.svg 1200w" '));
-        });
-
-        it('renders an expected html string without option', () => {
-            const props = {
-                alt: 'example',
-                widthDescriptor: {
-                    srcSet: [
-                        {descriptor: '360w', src: 'example-small.svg',},
-                        {descriptor: '720w', src: 'example-middle.svg',},
-                        {descriptor: '1200w', src: 'example-large.svg',},
-                    ],
-                    sizes: [
-                        {size: '100vw', mediaCondition: '(max-width: 30em)',},
-                        {size: '50vw', mediaCondition: '(max-width: 50em)',},
-                        {size: 'calc(33vw - 100px)', }, // should be default if mediaCondition is not given
-                    ]
-                },
-            };
-            const html = renderToString(createElement(ResponsiveImg, props));
-            assert(html.startsWith('<img'));
-            assert(html.includes(' alt="example" '));
-            assert(html.includes(' src="example-small.svg" '));
-            assert(html.includes(' srcset="example-small.svg 360w, example-middle.svg 720w, example-large.svg 1200w" '));
-            assert(html.includes(' sizes="(max-width: 30em) 100vw, (max-width: 50em) 50vw, calc(33vw - 100px)"'));
-        });
+    it('renders an expected html string without sizes option', () => {
+      const props = {
+        alt: 'example',
+        widthDescriptor: {
+          srcSet: [
+            {descriptor: '360w', src: 'example-small.svg',},
+            {descriptor: '720w', src: 'example-middle.svg',},
+            {descriptor: '1200w', src: 'example-large.svg',},
+          ],
+        },
+      };
+      const html = renderToString(createElement(ResponsiveImg, props));
+      assert(html.startsWith('<img'));
+      assert(html.includes(' alt="example" '));
+      assert(html.includes(' src="example-small.svg" '));
+      assert(html.includes(' srcset="example-small.svg 360w, example-middle.svg 720w, example-large.svg 1200w" '));
     });
 
-    describe('with pixelDescriptor', () => {
-        it('renders an expected html string', () => {
-            const props = {
-                alt: 'example',
-                pixelDescriptor: {
-                    srcSet: [
-                        {descriptor: '1x', src: 'example.svg'},
-                        {descriptor: '2x', src: 'example@2x.svg'},
-                    ],
-                },
-            };
-            const html = renderToString(createElement(ResponsiveImg, props));
-            assert(html.startsWith('<img'));
-            assert(html.includes(' alt="example" '));
-            assert(html.includes(' src="example.svg" '));
-            assert(html.includes(' srcset="example.svg 1x, example@2x.svg 2x" '));
-        });
+    it('renders an expected html string without option', () => {
+      const props = {
+        alt: 'example',
+        widthDescriptor: {
+          srcSet: [
+            {descriptor: '360w', src: 'example-small.svg',},
+            {descriptor: '720w', src: 'example-middle.svg',},
+            {descriptor: '1200w', src: 'example-large.svg',},
+          ],
+          sizes: [
+            {size: '100vw', mediaCondition: '(max-width: 30em)',},
+            {size: '50vw', mediaCondition: '(max-width: 50em)',},
+            {size: 'calc(33vw - 100px)',}, // should be default if mediaCondition is not given
+          ]
+        },
+      };
+      const html = renderToString(createElement(ResponsiveImg, props));
+      assert(html.startsWith('<img'));
+      assert(html.includes(' alt="example" '));
+      assert(html.includes(' src="example-small.svg" '));
+      assert(html.includes(' srcset="example-small.svg 360w, example-middle.svg 720w, example-large.svg 1200w" '));
+      assert(html.includes(' sizes="(max-width: 30em) 100vw, (max-width: 50em) 50vw, calc(33vw - 100px)"'));
     });
+  });
 
-    desribe('with both widthDescriptor and pixelDescriptor', () => {
-        it('should throw error', () => {
-            const props = {
-                alt: 'foo',
-                pixelDescriptor: {
-                    srcSet: [
-                        {descriptor: '1x', src: 'example.svg'}
-                    ],
-                },
-                widthDescriptor: {
-                    srcSet: [
-                        {descriptor: '360w', src: 'example-small.svg',}
-                    ],
-                },
-            };
-            const html = renderToString(createElement(ResponsiveImg, props));
-            // TODO: add test to catch console warning
-        });
+  describe('with pixelDescriptor', () => {
+    it('renders an expected html string', () => {
+      const props = {
+        alt: 'example',
+        pixelDescriptor: {
+          srcSet: [
+            {descriptor: '1x', src: 'example.svg'},
+            {descriptor: '2x', src: 'example@2x.svg'},
+          ],
+        },
+      };
+      const html = renderToString(createElement(ResponsiveImg, props));
+      assert(html.startsWith('<img'));
+      assert(html.includes(' alt="example" '));
+      assert(html.includes(' src="example.svg" '));
+      assert(html.includes(' srcset="example.svg 1x, example@2x.svg 2x" '));
     });
+  });
+
+  desribe('with both widthDescriptor and pixelDescriptor', () => {
+    it('should throw error', () => {
+      const props = {
+        alt: 'foo',
+        pixelDescriptor: {
+          srcSet: [
+            {descriptor: '1x', src: 'example.svg'}
+          ],
+        },
+        widthDescriptor: {
+          srcSet: [
+            {descriptor: '360w', src: 'example-small.svg',}
+          ],
+        },
+      };
+      const html = renderToString(createElement(ResponsiveImg, props));
+      // TODO: add test to catch console warning
+    });
+  });
 });
--- src/responsive_img.jsx
+++ src/responsive_img.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
 
 export default class ResponsiveImg extends React.Component {
-    static get propTypes() {
-        return {
-            alt: React.PropTypes.string.isRequired,
-            srcSet: React.PropTypes.shape({
-                '1x': React.PropTypes.string.isRequired,
-                '2x': React.PropTypes.string.isRequired,
-            }),
-            className: React.PropTypes.string,
-        };
-    }
+  static get propTypes() {
+    return {
+      alt: React.PropTypes.string.isRequired,
+      srcSet: React.PropTypes.shape({
+        '1x': React.PropTypes.string.isRequired,
+        '2x': React.PropTypes.string.isRequired,
+      }),
+      className: React.PropTypes.string,
+    };
+  }
 
-    getSrc() {
-        return this.props.srcSet['1x'];
-    }
+  getSrc() {
+    return this.props.srcSet['1x'];
+  }
 
-    buildSrcSet() {
-        return Object.keys(this.props.srcSet).map(size => {
-            return `${this.props.srcSet[size]} ${size}`;
-        });
-    }
+  buildSrcSet() {
+    return Object.keys(this.props.srcSet).map(size => {
+      return `${this.props.srcSet[size]} ${size}`;
+    });
+  }
 
-    render() {
-        return (
-            <img alt={this.props.alt}
-                 src={this.getSrc()}
-                 srcSet={this.buildSrcSet()}
-                 className={this.props.className} />
-        );
-    }
+  render() {
+    return (
+      <img alt={this.props.alt}
+           src={this.getSrc()}
+           srcSet={this.buildSrcSet()}
+           className={this.props.className}/>
+    );
+  }
 }
