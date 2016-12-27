6a172f84408a67dd68cdc0120cf2e5064f4558bd
Kenju Wagatsuma <ken901waga@gmail.com>
2016-12-28 01:08:32 +0900

    DRYer regexp const

--- src/matcher.jsx
+++ src/matcher.jsx
@@ -1,6 +1,7 @@
-const REGEXP_DESCRIPTOR_WIDTH = /^\d+(?:\.\d+)?w$/;
-const REGEXP_DESCRIPTOR_PIXEL = /^\d+(?:\.\d+)?x$/;
-const REGEXP_DESCRIPTOR_WIDTH_AND_PIXEL = /^\d+(?:\.\d+)?[wx]$/;
+const REGEXP_DECIMAL_NUMBER = /\d+(?:\.\d+)?/;
+const REGEXP_DESCRIPTOR_WIDTH = new RegExp(`^${REGEXP_DECIMAL_NUMBER.source}w$`);
+const REGEXP_DESCRIPTOR_PIXEL = new RegExp(`^${REGEXP_DECIMAL_NUMBER.source}x$`);
+const REGEXP_DESCRIPTOR_WIDTH_AND_PIXEL = new RegExp(`^${REGEXP_DECIMAL_NUMBER.source}[wx]$`);
 
 export default class Matcher {
   static matchWidthDescriptor(str) {
