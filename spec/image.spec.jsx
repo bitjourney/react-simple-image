057d93e4351cff9356d05153fd906f860f01a137
Kenju Wagatsuma <ken901waga@gmail.com>
2016-12-27 19:53:18 +0900

    Add validation logic not to render sizez props not when width descriptor only

--- spec/image.spec.jsx
+++ spec/image.spec.jsx
@@ -6,7 +6,7 @@ import { createElement } from 'react';
 import Image from 'image';
 
 describe('Image', () => {
-  describe('with widthDescriptor', () => {
+  describe('with width descriptor', () => {
     it('renders an expected html string without sizes option', () => {
       const props = {
         alt: 'example',
@@ -46,7 +46,7 @@ describe('Image', () => {
     });
   });
 
-  describe('with pixelDescriptor', () => {
+  describe('with pixel descriptor', () => {
     it('renders an expected html string', () => {
       const props = {
         alt: 'example',
@@ -61,9 +61,30 @@ describe('Image', () => {
       assert(html.includes(' src="example.png" '));
       assert(html.includes(' srcset="example.png 1x,example@2x.png 2x" '));
     });
+
+    it('should not render an expected html string with sizez', () => {
+      const props = {
+        alt: 'example',
+        srcSet: [
+          { descriptor: '1x', src: 'example.png' },
+          { descriptor: '2x', src: 'example@2x.png' },
+        ],
+        sizes: [
+          { size: '100vw', mediaCondition: '(max-width: 30em)' },
+          { size: '50vw', mediaCondition: '(max-width: 50em)' },
+          { size: 'calc(33vw - 100px)' },
+        ],
+      };
+      const html = renderToString(createElement(Image, props));
+      assert(html.startsWith('<img'));
+      assert(html.includes(' alt="example" '));
+      assert(html.includes(' src="example.png" '));
+      assert(html.includes(' srcset="example.png 1x,example@2x.png 2x" '));
+      assert(!html.includes(' sizes="(max-width: 30em) 100vw,(max-width: 50em) 50vw,calc(33vw - 100px)"'));
+    });
   });
 
-  describe('with both widthDescriptor and pixelDescriptor', () => {
+  describe('with both width descriptor and pixel descriptor', () => {
     it('should renders widthDescriptor over pixelDescriptor', () => {
       const props = {
         alt: 'example',
@@ -78,6 +99,23 @@ describe('Image', () => {
       assert(html.includes(' src="example-small.png" '));
       assert(html.includes(' srcset="example-small.png 360w" '));
       assert(!html.includes(' srcset="example.png 1x,example@2x.png 2x" '));
+    });
+  });
+
+  describe('with invalid descriptor', () => {
+    it('should throw error into console', () => {
+      const props = {
+        alt: 'example',
+        srcSet: [
+          { descriptor: 'invalid', src: 'example.png' },
+        ],
+      };
+      const html = renderToString(createElement(Image, props));
+      /*
+       * TODO: react gracefully show warning into console if props validation failed
+       * so currently there is no clean way to test invlid props
+       */
+      assert(html.startsWith('<img'));
     });
   });
 });
--- src/image.jsx
+++ src/image.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+const REGEXP_DESCRIPTOR_WIDTH = /(\d)+(\.\d+)?w$/;
+const REGEXP_DESCRIPTOR_WIDTH_AND_PIXEL = /(\d)+(\.\d+)?(w|x)$/;
+
 export default class Image extends React.Component {
   static get propTypes() {
     return {
@@ -7,7 +10,7 @@ export default class Image extends React.Component {
       className: React.PropTypes.string,
       srcSet: React.PropTypes.arrayOf(React.PropTypes.shape({
         descriptor: function(props, propName, componentName) {
-          if (!/(\d)+(\.\d+)?(w|x)/.test(props[ propName ])) {
+          if (!REGEXP_DESCRIPTOR_WIDTH_AND_PIXEL.test(props[ propName ])) {
             return new Error(`Invalid prop ${propName} supplied to '${componentName}'. Validation failed.`);
           }
         },
@@ -20,8 +23,15 @@ export default class Image extends React.Component {
     };
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasWidthDescriptorOnly : this.props.srcSet.every(srcSet => REGEXP_DESCRIPTOR_WIDTH.test(srcSet.descriptor))
+    };
+  }
+
   getSrc() {
-    return this.props.srcSet[ 0 ].src;
+    return this.props.srcSet[0].src;
   }
 
   buildSrcSet() {
@@ -29,7 +39,7 @@ export default class Image extends React.Component {
   }
 
   buildSizes() {
-    if (this.props.sizes) {
+    if (this.props.sizes && this.state.hasWidthDescriptorOnly) {
       return this.props.sizes.map((size) => {
         if (size.mediaCondition) {
           return `${size.mediaCondition} ${size.size}`;
