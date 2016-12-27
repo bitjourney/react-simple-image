165abbf4661d60b3a3f89a11b4cda0a2cecf66ce
Kenju Wagatsuma <ken901waga@gmail.com>
2016-12-28 00:48:02 +0900

    Fix matchDescriptor regexp

--- src/matcher.jsx
+++ src/matcher.jsx
@@ -1,7 +1,7 @@
 // TODO: fix regexp after reading book
 const REGEXP_DESCRIPTOR_WIDTH = /^\d+(?:\.\d+)?w$/;
 const REGEXP_DESCRIPTOR_PIXEL = /^\d+(?:\.\d+)?x$/;
-const REGEXP_DESCRIPTOR_WIDTH_AND_PIXEL = /(\d)+(\.\d+)?(w|x)$/;
+const REGEXP_DESCRIPTOR_WIDTH_AND_PIXEL = /^\d+(?:\.\d+)?[wx]$/;
 
 export default class Matcher {
   static matchWidthDescriptor(str) {
