380c56f28c98ac3cbb3d1369f3f6e41063a3fb27
Kenju Wagatsuma <ken901waga@gmail.com>
2016-12-28 00:44:45 +0900

    Add spec for Matcher#matchPixelDescriptor

--- spec/matcher.spec.jsx
+++ spec/matcher.spec.jsx
@@ -20,6 +20,58 @@ describe('Matcher', () => {
         '1x',
         '2x',
         'foo2w',
+        '2w2',
+        'invalid',
+        true,
+        null,
+        undefined,
+      ];
+      invalidInputs.map(str => assert(!Matcher.matchWidthDescriptor(str)));
+    });
+  });
+
+  describe('#matchPixelDescriptor', () => {
+    it('should match with width descriptor', () => {
+      const validInputs = [
+        '0x',
+        '1x',
+        '1.5x',
+        '2x',
+      ];
+      validInputs.map(str => assert(Matcher.matchPixelDescriptor(str)));
+    });
+
+    it('should not match with invalid descriptor', () => {
+      const invalidInputs = [
+        '1w',
+        '360w',
+        'foo2x',
+        '2x2',
+        'invalid',
+        true,
+        null,
+        undefined,
+      ];
+      invalidInputs.map(str => assert(!Matcher.matchPixelDescriptor(str)));
+    });
+  });
+
+  describe('#matchWidthDescriptor', () => {
+    it('should match with width descriptor', () => {
+      const validInputs = [
+        '0w',
+        '360w',
+        '1200w',
+        '33.3w',
+      ];
+      validInputs.map(str => assert(Matcher.matchWidthDescriptor(str)));
+    });
+
+    it('should not match with invalid descriptor', () => {
+      const invalidInputs = [
+        '1x',
+        '2x',
+        'foo2w',
         'invalid',
         true,
         null,
--- src/matcher.jsx
+++ src/matcher.jsx
@@ -1,6 +1,6 @@
 // TODO: fix regexp after reading book
 const REGEXP_DESCRIPTOR_WIDTH = /^\d+(?:\.\d+)?w$/;
-const REGEXP_DESCRIPTOR_PIXEL = /(\d)+(\.\d+)?x$/;
+const REGEXP_DESCRIPTOR_PIXEL = /^\d+(?:\.\d+)?x$/;
 const REGEXP_DESCRIPTOR_WIDTH_AND_PIXEL = /(\d)+(\.\d+)?(w|x)$/;
 
 export default class Matcher {
