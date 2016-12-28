ddc01d7bd202b94c86f417ccdcd8f22c749cae65
Kenju Wagatsuma <ken901waga@gmail.com>
2016-12-28 10:48:10 +0900

    DRYer spec inputs array const

--- spec/matcher.spec.jsx
+++ spec/matcher.spec.jsx
@@ -4,85 +4,68 @@ import assert from 'power-assert';
 import Matcher from 'matcher';
 
 describe('Matcher', () => {
+  const validWidths = [
+    '0w',
+    '360w',
+    '1200w',
+    '33.3w',
+  ];
+
+  const validPixels = [
+    '0x',
+    '1x',
+    '1.5x',
+    '2x',
+  ];
+
+  const invalidWidths = [
+    'foo2w',
+    '2w2',
+  ];
+
+  const invalidPixels = [
+    'foo2x',
+    '2x2',
+  ];
+
+  const invalidDescriptor = [
+    'invalid',
+    true,
+    null,
+    undefined,
+  ];
+
   describe('#matchDescriptor', () => {
     it('should match with both width and pixel descriptor', () => {
-      const validInputs = [
-        '0w',
-        '360w',
-        '1200w',
-        '33.3w',
-        '0x',
-        '1x',
-        '1.5x',
-        '2x',
-      ];
-      validInputs.map(str => assert(Matcher.matchDescriptor(str)));
+      validWidths.concat(validPixels)
+        .map(str => assert(Matcher.matchDescriptor(str)));
     });
 
     it('should not match with invalid descriptor', () => {
-      const invalidInputs = [
-        'foo2w',
-        '2w2',
-        'foo2x',
-        '2x2',
-        'invalid',
-        true,
-        null,
-        undefined,
-      ];
-      invalidInputs.map(str => assert(!Matcher.matchDescriptor(str)));
+      invalidDescriptor.concat(invalidPixels, invalidWidths)
+        .map(str => assert(!Matcher.matchDescriptor(str)));
     });
   });
 
   describe('#matchWidthDescriptor', () => {
     it('should match with width descriptor', () => {
-      const validInputs = [
-        '0w',
-        '360w',
-        '1200w',
-        '33.3w',
-      ];
-      validInputs.map(str => assert(Matcher.matchWidthDescriptor(str)));
+      validWidths.map(str => assert(Matcher.matchWidthDescriptor(str)));
     });
 
     it('should not match with invalid descriptor', () => {
-      const invalidInputs = [
-        '1x',
-        '2x',
-        'foo2w',
-        '2w2',
-        'invalid',
-        true,
-        null,
-        undefined,
-      ];
-      invalidInputs.map(str => assert(!Matcher.matchWidthDescriptor(str)));
+      invalidDescriptor.concat(validPixels, invalidWidths)
+        .map(str => assert(!Matcher.matchWidthDescriptor(str)));
     });
   });
 
   describe('#matchPixelDescriptor', () => {
     it('should match with pixel descriptor', () => {
-      const validInputs = [
-        '0x',
-        '1x',
-        '1.5x',
-        '2x',
-      ];
-      validInputs.map(str => assert(Matcher.matchPixelDescriptor(str)));
+      validPixels.map(str => assert(Matcher.matchPixelDescriptor(str)));
     });
 
     it('should not match with invalid descriptor', () => {
-      const invalidInputs = [
-        '1w',
-        '360w',
-        'foo2x',
-        '2x2',
-        'invalid',
-        true,
-        null,
-        undefined,
-      ];
-      invalidInputs.map(str => assert(!Matcher.matchPixelDescriptor(str)));
+      invalidDescriptor.concat(validWidths, invalidPixels)
+        .map(str => assert(!Matcher.matchPixelDescriptor(str)));
     });
   });
 });
